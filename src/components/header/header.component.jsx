2111d1b77470aa1723d985aaf39713f0d911ec7d
kevin paul shaju <kvnpul155@gmail.com>
2021-05-28 01:20:29 +0530

    added cart icon and cart dropdown  functions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { connect } from "react-redux";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -20,12 +22,15 @@ const Header = ({ currentUser }) => (
         CONTACT
       </Link>
       {currentUser ? (<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>) : (<Link className="option" to="/signin">SIGN IN</Link>)}
+      <CartIcon></CartIcon>
     </div>
+    {!hidden && <CartDropdown></CartDropdown>}
   </div>
 );
 
-const mapSateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapSateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapSateToProps)(Header);
